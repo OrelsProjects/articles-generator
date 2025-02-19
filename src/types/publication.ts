95d5ae2979fab15586d2fa5c036c04c6341d3bb7
orelsprojects <orelsmail@gmail.com>
2025-02-19 10:53:30 +0200

    Update publication response type and caniuse-lite package
    - Remove `didUserSee` from ideas in PublicationResponse type
    - Upgrade caniuse-lite to version 1.0.30001700
    - Add license field to caniuse-lite package

--- src/types/publication.ts
+++ src/types/publication.ts
@@ -13,6 +13,6 @@ export interface PublicationResponse {
   image: string | null;
   title: string | null;
   description: string | null;
-  ideas: Idea[];
+  ideas: Omit<Idea, "didUserSee">[];
   url: string;
 }
