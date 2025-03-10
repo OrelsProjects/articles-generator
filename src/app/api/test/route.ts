e1d5a53893d1c244c9cd7ee106c326f01b32c8b8
orelsprojects <orelsmail@gmail.com>
2025-03-10 16:33:17 +0200

    remove api test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,12 +2,5 @@ import prisma from "@/app/api/_db/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const allUserMetadata = await prisma.userMetadata.findMany();
-  await prisma.userMetadata.updateMany({
-    data: {
-      plan: null,
-    },
-  });
-  const allUserMetadataNew = await prisma.userMetadata.findMany();
   return NextResponse.json({ message: "Updated" });
 }
