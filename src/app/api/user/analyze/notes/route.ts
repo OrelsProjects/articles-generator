af888eaea873a7c14bf20f3657e0ac44116a38d4
orelsprojects <orelsmail@gmail.com>
2025-03-16 17:20:29 +0200

    fIX

--- src/app/api/user/analyze/notes/route.ts
+++ src/app/api/user/analyze/notes/route.ts
@@ -7,7 +7,7 @@ import { parseJson } from "@/lib/utils/json";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
