a489ae450056b35c42c91248f8ad827c1d6b7af1
orelsprojects <orelsmail@gmail.com>
2025-03-30 23:29:25 +0300

    Fix 2.7

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -75,7 +75,7 @@ export const HeroSection = () => {
                 {[
                   "VIRAL NOTES RESEARCH TOOL",
                   "PERSONALIZED GENERATED NOTES",
-                  "X2.4 MORE SUBSCRIBERS",
+                  "X2.7 MORE SUBSCRIBERS",
                 ].map((feature, index) => (
                   <li
                     key={index}
