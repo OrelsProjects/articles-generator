15bd1858cdb820aee471403244c00cea7afc607d
orelsprojects <orelsmail@gmail.com>
2025-04-19 22:09:13 +0300

    ANother test

--- src/app/api/user/notes/send/route.ts
+++ src/app/api/user/notes/send/route.ts
@@ -193,6 +193,17 @@ export async function POST(request: NextRequest) {
             Referer: "https://substack.com/home",
             "Referrer-Policy": "strict-origin-when-cross-origin",
             Cookie: `substack.sid=${cookie.value}`,
+            "sec-ch-ua":
+              '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            priority: "u=1, i",
+            "user-agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+            referer: "https://substack.com/home",
           },
           body: JSON.stringify(messageData),
         },
