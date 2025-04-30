186ccaf8285486ddaaac7cd0c0d312c4fa956f20
orelsprojects <orelsmail@gmail.com>
2025-04-30 11:24:14 +0300

    Fix extension

--- src/app/api/v1/extension/schedule/[scheduleId]/route.ts
+++ src/app/api/v1/extension/schedule/[scheduleId]/route.ts
@@ -10,11 +10,6 @@ export async function GET(
   _: NextRequest,
   { params }: { params: { scheduleId: string } },
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
   try {
     const { scheduleId } = params;
     const note = await getNoteByScheduleId(scheduleId);
