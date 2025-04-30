f70b8043d3f890fcf3ddeb7a7f561c78666e23c1
orelsprojects <orelsmail@gmail.com>
2025-04-30 13:05:10 +0300

    add logs

--- src/app/providers/AffiliateProvider.tsx
+++ src/app/providers/AffiliateProvider.tsx
@@ -5,8 +5,6 @@ import Script from "next/script";
 import { useSearchParams } from "next/navigation";
 import { Logger } from "@/logger";
 
-const API_KEY = process.env.NEXT_PUBLIC_AFFILIATE_API_KEY;
-
 export default function AffiliateProvider({
   children,
 }: {
@@ -18,6 +16,7 @@ export default function AffiliateProvider({
 
   useEffect(() => {
     // Check if rewardful is defined in the global scope
+    console.log("Type of rewardful", typeof rewardful);
     if (typeof window !== "undefined" && typeof rewardful === "function") {
       Logger.info(`[REFERRAL] Checking for referral`);
       rewardful("ready", function () {
@@ -39,7 +38,7 @@ export default function AffiliateProvider({
 
   return (
     <>
-      <Script src="https://r.wdfl.co/rw.js" data-rewardful={API_KEY} />
+      <Script src="https://r.wdfl.co/rw.js" data-rewardful="00b47f" />
       <Script id="rewardful-queue" strategy="beforeInteractive">
         {`(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`}
       </Script>
