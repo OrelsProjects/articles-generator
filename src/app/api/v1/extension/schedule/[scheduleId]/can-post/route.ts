12ed313906ff52ed7f4401980c2e67034d2cb6b5
orelsprojects <orelsmail@gmail.com>
2025-05-05 17:01:16 +0300

    add logs

--- src/app/api/v1/extension/schedule/[scheduleId]/can-post/route.ts
+++ src/app/api/v1/extension/schedule/[scheduleId]/can-post/route.ts
@@ -1,28 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/app/api/_db/db";
+import loggerServer from "@/loggerServer";
 
 export async function POST(
   request: NextRequest,
   { params }: { params: { scheduleId: string } },
 ) {
   const { scheduleId } = params;
-  const schedule = await prisma.scheduledNote.findUnique({
-    where: {
-      id: scheduleId,
-    },
-  });
+  try {
+    loggerServer.info("[CAN-POST] scheduleId: " + scheduleId);
+    const schedule = await prisma.scheduledNote.findUnique({
+      where: {
+        id: scheduleId,
+      },
+    });
 
-  if (!schedule) {
-    return NextResponse.json({ error: "Schedule not found" }, { status: 404 });
-  }
+    if (!schedule) {
+      loggerServer.error("[CAN-POST] Schedule not found: " + scheduleId);
+      return NextResponse.json(
+        { error: "Schedule not found" },
+        { status: 404 },
+      );
+    }
 
-  // If the schedule is in the past, more than 10 minutes ago, return false
-  if (schedule.scheduledAt <= new Date(Date.now() - 10 * 60 * 1000)) {
-    return NextResponse.json(
-      { canPost: false, error: "Schedule was missed" },
-      { status: 200 },
-    );
-  }
+    loggerServer.info("[CAN-POST] schedule: " + JSON.stringify(schedule));
+
+    // If the schedule is in the past, more than 10 minutes ago, return false
+    if (schedule.scheduledAt <= new Date(Date.now() - 10 * 60 * 1000)) {
+      return NextResponse.json(
+        { canPost: false, error: "Schedule was missed" },
+        { status: 200 },
+      );
+    }
 
     return NextResponse.json({ canPost: true }, { status: 200 });
+  } catch (error) {
+    loggerServer.error("[CAN-POST] Error: " + error);
+    return NextResponse.json(
+      { canPost: false, error: "Internal server error" },
+      { status: 500 },
+    );
+  }
 }
