750f9d5b0e5aaebcc3b2e4854aa233d6c346c0b4
orelsprojects <orelsmail@gmail.com>
2025-05-12 13:04:29 +0300

    Fix mails

--- src/lib/mail/mail.ts
+++ src/lib/mail/mail.ts
@@ -21,7 +21,7 @@ export const sendMail = async ({
   sendInDevelopment = false,
 }: SendEmailOptions) => {
   const env = process.env.NODE_ENV;
-  if (env === "production" && !sendInDevelopment) {
+  if (env !== "production" && !sendInDevelopment) {
     console.log(
       `[MAIL] Not sending mail in production: ${subject}, to: ${to}, from: ${from}, cc: ${cc}, template: ${template}`,
     );
