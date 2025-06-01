c5cd0fcf941944e5a2bff9cc6c68e5ffb29fe959
orelsprojects <orelsmail@gmail.com>
2025-06-01 13:25:32 +0300

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,36 @@
-import loggerEdge from "@/loggerEdge";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    const headers = Object.fromEntries(request.headers.entries());
-    const body = await request.json();
-    loggerEdge.info("API Request Log:", {
-      endpoint: request.nextUrl.pathname,
-      method: request.method,
-      headers: JSON.stringify(headers, null, 2),
-      body: JSON.stringify(body, null, 2),
-      userId: "middleware",
-    });
-  }
+  try {
+    if (request.nextUrl.pathname.startsWith("/api")) {
+      let body: string | null = null;
+      let headers: Record<string, string> | null = null;
+      try {
+        body = await request.json();
+      } catch (error) {
+        body = null;
+      }
 
-  return NextResponse.next();
+      try {
+        headers = Object.fromEntries(request.headers.entries());
+      } catch (error) {
+        headers = null;
+      }
+
+      console.log("API Request Log:", {
+        endpoint: request.nextUrl.pathname,
+        method: request.method,
+        headers: JSON.stringify(headers, null, 2),
+        body: JSON.stringify(body, null, 2),
+        userId: "middleware",
+      });
+    }
+  } catch (error) {
+    console.error("Error logging API request:", error);
+  } finally {
+    return NextResponse.next();
+  }
 }
 
 export const config = {
