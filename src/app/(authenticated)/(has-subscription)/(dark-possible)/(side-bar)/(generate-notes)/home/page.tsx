de14e750ca2a0a44465eb432a78c5b0530a4508f
orelsprojects <orelsmail@gmail.com>
2025-06-09 16:11:31 +0300

    add image og

--- src/app/(authenticated)/(has-subscription)/(dark-possible)/(side-bar)/(generate-notes)/home/page.tsx
+++ src/app/(authenticated)/(has-subscription)/(dark-possible)/(side-bar)/(generate-notes)/home/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import InspirationGrid from "@/components/home/inspiration-notes-grid";
-import { EventTracker } from "@/eventTracker";
 
 export default function HomePage() {
-  // Track how long on this page
-  useEffect(() => {
-    const date = new Date();
-
-    return () => {
-      const endDate = new Date();
-      const duration = endDate.getTime() - date.getTime();
-      EventTracker.track("page_view", {
-        page: "home",
-        duration: `${duration / 1000}s`,
-      });
-    };
-  }, []);
-
   return (
     <div className="w-full h-full bg-background flex justify-center items-start relative z-20">
       <InspirationGrid />
