2754f34bb0223fb3ee9a8f935c151a862a7e2035
orelsprojects <orelsmail@gmail.com>
2025-06-11 14:30:08 +0300

    Fix

--- src/app/(authenticated)/(has-subscription)/(dark-possible)/(side-bar)/(generate-notes)/me/page.tsx
+++ src/app/(authenticated)/(has-subscription)/(dark-possible)/(side-bar)/(generate-notes)/me/page.tsx
@@ -1,40 +1,41 @@
 "use client";
 
-import React from "react";
-import UserWriterProfile from "@/components/user-writer-profile";
-import { useMePage } from "@/lib/hooks/useMePage";
+// import React from "react";
+// import UserWriterProfile from "@/components/user-writer-profile";
+// import { useMePage } from "@/lib/hooks/useMePage";
 
 export default function MyProfilePage() {
-  const {
-    isLoading,
-    fetchNextPage,
-    hasMore,
-    error,
-    isLoadingMore,
-    isLoadingOrderBy,
-    isLoadingOrderDirection,
-    data,
-    updateOrderBy,
-    updateOrderDirection,
-    orderBy,
-    orderDirection,
-  } = useMePage();
+  // const {
+  //   isLoading,
+  //   fetchNextPage,
+  //   hasMore,
+  //   error,
+  //   isLoadingMore,
+  //   isLoadingOrderBy,
+  //   isLoadingOrderDirection,
+  //   data,
+  //   updateOrderBy,
+  //   updateOrderDirection,
+  //   orderBy,
+  //   orderDirection,
+  // } = useMePage();
 
   return (
-    <UserWriterProfile
-      writer={data}
-      isLoading={isLoading}
-      fetchNextPage={fetchNextPage}
-      hasMore={hasMore}
-      error={error}
-      isLoadingMore={isLoadingMore}
-      isLoadingOrderBy={isLoadingOrderBy}
-      isLoadingOrderDirection={isLoadingOrderDirection}
-      updateOrderBy={updateOrderBy}
-      updateOrderDirection={updateOrderDirection}
-      orderBy={orderBy}
-      orderDirection={orderDirection}
-    />
+    // <UserWriterProfile
+    //   writer={data}
+    //   isLoading={isLoading}
+    //   fetchNextPage={fetchNextPage}
+    //   hasMore={hasMore}
+    //   error={error}
+    //   isLoadingMore={isLoadingMore}
+    //   isLoadingOrderBy={isLoadingOrderBy}
+    //   isLoadingOrderDirection={isLoadingOrderDirection}
+    //   updateOrderBy={updateOrderBy}
+    //   updateOrderDirection={updateOrderDirection}
+    //   orderBy={orderBy}
+    //   orderDirection={orderDirection}
+    // />
+    null
   );
 }
 
